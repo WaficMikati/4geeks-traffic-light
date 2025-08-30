77174a120a68491f5901461a03e8e04ef938f59e
WaficMikati <wrmikati@proton.me>
2025-08-30 03:40:02 -0500

    added preview image

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-// const importedImages = import.meta.glob('./assets/*.png', {
-//   eager: true,
-//   import: 'default'
-// })
-//
-// const images = {}
-// for (const path in importedImages) {
-//   const name = path.split('/').pop().split('.')[0]
-//   images[name] = importedImages[path]
-// }
+const importedImages = import.meta.glob('./assets/*.png', {
+  eager: true,
+  import: 'default'
+})
+
+const images = {}
+for (const path in importedImages) {
+  const name = path.split('/').pop().split('.')[0]
+  images[name] = importedImages[path]
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
